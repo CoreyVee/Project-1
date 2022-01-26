513578cb1cfb061e8fd1822872aa12216b6ff3ed
CoreyVee <38364203+CoreyVee@users.noreply.github.com>
2022-01-26 16:10:29 -0600

    Dropdown menu now clickable and hides
    Dropdown menu is now clickable with js and doesn't have bullet points. Format doesn't overlap until 370px across.

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 $(() => {
     console.log( "html is done loading, page ready!" );
     $('#Drop_menu_icon').click(function(){
-		$('#Drop_menu_icon').toggleClass('expand');
+		$('.DropDownMenu').toggleClass('show');
 	});
 });
