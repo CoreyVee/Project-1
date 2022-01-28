50287ac26d2b27d0391b22782ab8235a1bdbf4ca
CoreyVee <38364203+CoreyVee@users.noreply.github.com>
2022-01-28 11:33:06 -0600

    Updated anchor tags for navigation
    Updated some of the anchor tags so that they can be used to navigate to specific sections of the page.

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
 $(() => {
     console.log( "html is done loading, page ready!" );
     $('#Drop_menu_icon').click(function(){
-		$('.DropDownMenu').toggleClass('show');
-    
+		$('.DropDownMenu').toggleClass('show')
 	});
+ 
+// get nav
+  // const myNav = document.getElementById("header")
+  // // get offset position of nav
+  // const sticky = myNav.offsetTop
+
+  // function stickyNav() {
+  //   if (window.pageYOffset >= sticky) {
+  //     myNav.classList.add("sticky")
+  //   } else {
+  //     myNav.classList.remove("sticky");
+  //   }
+  // }
+
+  // window.onscroll = stickyNav()
+  
 });
